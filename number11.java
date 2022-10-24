7f45ad962014a0269099a6a056f28b1c3b95d47b
xxphjm <s43926027@gmail.com>
2022-10-24 23:32:09 +0800

    first commit

--- number11.java
+++ number11.java
@@ -10,7 +10,7 @@ public class number11 {
     }
     public static  String get_con(int month, int date){
         int [] dates = {21, 19, 21, 21, 22, 22, 23, 24, 24, 24, 23, 22};
-        String[] con ={"摩羯座", "水瓶座 ", "雙魚座", "牡羊座", "金牛座", "雙子座", "巨蟹座", "獅子座", "處女座", "天秤座", "天蝎座", "射手座"};
+        String[] con ={"摩羯座", "水瓶", "雙魚座", "牡羊座", "金牛座", "雙子座", "巨蟹座", "獅子座", "處女座", "天秤座", "天蝎座", "射手座"};
         if (date < dates[month-1]) {
             return con[month-1]; 
         }
